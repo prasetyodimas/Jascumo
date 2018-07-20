10cb4ee9f61b6578d0784beeaa6195a5aea4bcb7
Dimas Prasetyo <dimasprasetyo485@gmail.com>
2018-07-20 10:11:30 +0700

    add new function

--- modules/backend/proses_booking.php
+++ modules/backend/proses_booking.php
@@ -254,64 +254,6 @@ if ($act=='booking') {
 		echo "<meta http-equiv=refresh content=0;url=$site"."index.php?booking>";
 	}	
 
-	
-
-
-	// proses sending to mailer	
-	$mail = new PHPMailer(true);                               // Passing `true` enables exceptions
-	try {
-	    //Server settings
-	    $mail->isSMTP();                                       // Set mailer to use SMTP
-	    $mail->SMTPDebug  = 2;                                 // Enable verbose debug output
-	    $mail->Host       = 'smtp.gmail.com';                  // Specify main and backup SMTP servers
-	    $mail->SMTPAuth   = true;                              // Enable SMTP authentication
-	    $mail->Username   = 'carwashcrowns@gmail.com';         // SMTP username
-	    $mail->Password   = 'crowncar123';                     // SMTP password
-	    $mail->SMTPSecure = 'tls';                             // Enable TLS encryption, `ssl` also accepted
-	    $mail->Port       = 587;                               // TCP port to connect to
-
-	    //Recipients
-	    $mail->setFrom('carwashcrowns@gmail.com', 'CrownCars Wash');
-	    $mail->addAddress(''.$getMember['email_member'].'', ''.$getMember['nama_member'].'');     // Add a recipient
-	    $mail->addReplyTo('carwashcrowns@gmail.com', 'Information / Custtomer Service');
-
-	    //Attachments
-	    // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-	    // $mail->addAttachment('../../frontend/logo/crown-cars.png', 'crownbranding.jpg');    // Optional name
-
-	    //Content
-	    $mail->isHTML(true);                                     // Set email format to HTML
-	    $mail->Subject = 'Crown Carswash Solution';
-	    // Compose a simple HTML email message
-	    $mail->Body    = '<h3 style="color:#000;">Dear Yth <span style="color:#eac702;">'.$getMember['nama_member'].'</span></h3>'
-	                     .'<p>Terimakasih telah menggunakan layanan dan kepercayaan kami <span style="font-size:12px;font-style:italic;"> crowncarswash solution </span></p>'
-	                     .'<p>Booking Information :</p>'
-	                     .'<table style="border:1px solid #b9b9b9;padding:10px;">
-	                            <thead>
-	                                <tr>Kode Booking       : '.$no_nota.' </tr>
-	                                <tr>Id Member          : '.$id_member.' </tr>
-	                                <tr>Tanggal Pesan      : '.$tgl_pesan.' </tr>
-	                                <tr>Nama Pemesan       : '.$getMember['nama_member'].' </tr>
-	                                <tr>Alamat             : '.$getMember['alamat_member'].'</tr>
-	                                <tr>Notelp / Handphone : '.$getMember['notelp_member'].' </tr>
-	                                <tr>Nama Kendaraan	   : '.$transact['nama_kendaran'].' </tr>
-	                                <tr>Subtotal		   : Rp.'.number_format($countTrans).',-'.' </tr>
-	                            </thead>
-	                       </table>'
-	                    .'<h3>Nomor Antrian Sementara Anda <span style"font-size:15px;">('.$no_antrian.')</span></h3>'
-	                    .'<p>Ketentuan : Tunjukan Kode Booking ini kepada kami sebagai bukti konfirmasi bahwa anda telah memesan jika anda ingin proses mencuci mobil. 
-	                      jika anda datang terlambat otomatis nomer antrian akan kami ganti untuk itu bawa bukti ini sebagai konfirmasi no antrian.</p>'
-	                    .'<h4>Crown Carwash We Serve With Profesional Way Open 9 AM - Closed 6 PM <span style="font-size:11px;font-style:italic;"> Please Contact Us We Care About You :) Cheers </span></h4>'
-	                    .'<p style="margin-bottom:1em;">Warm Regard</p>'
-	                    .'</br>'
-	                    .'<p>Customer Services</p>'
-	                     ;
-	    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-	    $mail->send();
-	    echo 'Message has been sent';
-	}catch (Exception $e) {
-	    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-	}
 
 }elseif($act=='check_bookValidation'){
 	
